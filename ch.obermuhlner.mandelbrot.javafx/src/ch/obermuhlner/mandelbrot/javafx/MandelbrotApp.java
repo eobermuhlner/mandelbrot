373850c38d83a70ce5a71356f062f9e0ec6d4c34
Eric Obermühlner <eobermuhlner@gmail.com>
2017-05-03 22:32:02 +0200

    handle zoom events on touch screen

--- ch.obermuhlner.mandelbrot.javafx/src/ch/obermuhlner/mandelbrot/javafx/MandelbrotApp.java
+++ ch.obermuhlner.mandelbrot.javafx/src/ch/obermuhlner/mandelbrot/javafx/MandelbrotApp.java
@@ -1,12 +1,19 @@
 package ch.obermuhlner.mandelbrot.javafx;
 
+import java.text.DecimalFormat;
+
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -23,7 +30,9 @@ public class MandelbrotApp extends Application {
 			PALETTE[i] = Color.hsb(correctedFactor * 360, 1.0, 1.0 - correctedFactor);
 		}
 	}
-	
+
+	private static final DecimalFormat DOUBLE_FORMAT = new DecimalFormat("##0.00000000");
+
 	private DoubleProperty xCenterProperty = new SimpleDoubleProperty(0.0);
 	private DoubleProperty yCenterProperty = new SimpleDoubleProperty(0.0);
 	private DoubleProperty radiusProperty = new SimpleDoubleProperty(2.0);
@@ -33,17 +42,47 @@ public class MandelbrotApp extends Application {
 		Group root = new Group();
 		Scene scene = new Scene(root);
 		
-		double height = 800;
-		double width = 800;
-		Canvas canvas = new Canvas(width, height);
-		drawMandelbrot(canvas, 1);
+		BorderPane borderPane = new BorderPane();
+		root.getChildren().add(borderPane);
 		
-		root.getChildren().add(canvas);
+		Node toolbar = createToolbar();
+		borderPane.setTop(toolbar);
+
+		Canvas canvas = createMandelbrotCanvas();
+		borderPane.setCenter(canvas);
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+
+	private Canvas createMandelbrotCanvas() {
+		double height = 800;
+		double width = 800;
+		
+		Canvas canvas = new Canvas(width, height);
+		drawMandelbrot(canvas, 1);
 
 		setupCanvasEventHandlers(canvas);
+
+		return canvas;
+	}
+
+	private Node createToolbar() {
+		HBox toolbar = new HBox();
+		
+		TextField xCenterTextField = new TextField();
+		toolbar.getChildren().add(xCenterTextField);
+		Bindings.bindBidirectional(xCenterTextField.textProperty(), xCenterProperty, DOUBLE_FORMAT);
+		
+		TextField yCenterTextField = new TextField();
+		toolbar.getChildren().add(yCenterTextField);
+		Bindings.bindBidirectional(yCenterTextField.textProperty(), yCenterProperty, DOUBLE_FORMAT);
+		
+		TextField radiusTextField = new TextField();
+		toolbar.getChildren().add(radiusTextField);
+		Bindings.bindBidirectional(radiusTextField.textProperty(), radiusProperty, DOUBLE_FORMAT);
+		
+		return toolbar;
 	}
 
 	double lastMouseDragX;
@@ -70,10 +109,19 @@ public class MandelbrotApp extends Application {
 			translateMandelbrot(canvas, deltaX, deltaY, 1);
 		});
 		
+		canvas.setOnZoom(event -> {
+			zoomMandelbrot(canvas, 1.0 / event.getZoomFactor(), 2);
+		});
+		canvas.setOnZoomFinished(event -> {
+			drawMandelbrot(canvas, 1);
+		});
+		
 		canvas.setOnScroll(event -> {
-			double deltaY = event.getDeltaY();
-			
-			zoomMandelbrot(canvas, deltaY, 1);
+			if (!event.isDirect()) {
+				double deltaY = event.getDeltaY();
+				
+				zoomScrollMandelbrot(canvas, deltaY, 1);
+			}
 		});
 	}
 
@@ -90,7 +138,7 @@ public class MandelbrotApp extends Application {
 		drawMandelbrot(canvas, pixelSize);
 	}
 
-	private void zoomMandelbrot(Canvas canvas, double deltaPixelY, int pixelSize) {
+	private void zoomScrollMandelbrot(Canvas canvas, double deltaPixelY, int pixelSize) {
 		double pixelHeight = canvas.getHeight();
 	
 		double deltaZ = deltaPixelY / pixelHeight * 2.0;
@@ -100,6 +148,11 @@ public class MandelbrotApp extends Application {
 		drawMandelbrot(canvas, pixelSize);
 	}
 
+	private void zoomMandelbrot(Canvas canvas, double zoomFactor, int pixelSize) {
+		radiusProperty.set(radiusProperty.get() * zoomFactor);
+
+		drawMandelbrot(canvas, pixelSize);
+	}
 
 	private void drawMandelbrot(Canvas canvas, int pixelSize) {
 		GraphicsContext gc = canvas.getGraphicsContext2D();
