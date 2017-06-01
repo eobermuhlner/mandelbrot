6668cfb819bd7ee37f7dc84317968c0242781b0a
Eric Obermühlner <eobermuhlner@gmail.com>
2017-06-01 17:57:26 +0200

    delete obsolete command line project

--- ch.obermuhlner.mandelbrot.imagegen/run_ffmpeg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ffmpeg -y -r 10 -start_number 0 -i mandelbrot%04d.png -s 800x800 -vcodec mpeg4 -q:v 1 mandelbrot.mp4
-
--- ch.obermuhlner.mandelbrot.imagegen/src/ch/obermuhlner/mandelbrot/imagegen/Palette.java
+++
@@ -1,7 +0,0 @@
-package ch.obermuhlner.mandelbrot.imagegen;
-
-import java.awt.Color;
-
-public interface Palette {
-	Color getColor(int iterations);
-}
