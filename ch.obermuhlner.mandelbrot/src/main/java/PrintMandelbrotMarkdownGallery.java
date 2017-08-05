ee7a210336f42376f2ad2fad7ca2c02aff0d5a7c
Eric Obermühlner <eobermuhlner@gmail.com>
2017-08-05 13:21:16 +0200

    print mandelbrot markdown gallery for github

---
+++ ch.obermuhlner.mandelbrot/src/main/java/PrintMandelbrotMarkdownGallery.java
@@ -0,0 +1,9 @@
+
+public class PrintMandelbrotMarkdownGallery {
+
+	public static void main(String[] args) {
+		for (String file : args) {
+			System.out.println("[![](images/192x120/" + file + ")](images/1920x1200/" + file + "?raw=true)");
+		}
+	}
+}
