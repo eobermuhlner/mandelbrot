f882390f4571ffc1cf0dfbcab825f0e97f946e73
Eric Obermühlner <eobermuhlner@gmail.com>
2018-07-08 13:16:16 +0200

    simple nice zoom around

--- ch.obermuhlner.mandelbrot.cli.zoom/src/main/java/ch/obermuhlner/mandelbrot/cli/zoom/MandelbrotMovie.java
+++ ch.obermuhlner.mandelbrot.cli.zoom/src/main/java/ch/obermuhlner/mandelbrot/cli/zoom/MandelbrotMovie.java
@@ -3,7 +3,8 @@ package ch.obermuhlner.mandelbrot.cli.zoom;
 import ch.obermuhlner.mandelbrot.palette.MixPalette;
 import ch.obermuhlner.mandelbrot.palette.Palette;
 import ch.obermuhlner.mandelbrot.palette.PaletteFactory;
-import ch.obermuhlner.mandelbrot.palette.PaletteType;
+import ch.obermuhlner.mandelbrot.poi.PointOfInterest;
+import ch.obermuhlner.mandelbrot.poi.StandardPointsOfInterest;
 
 import java.io.File;
 import java.math.BigDecimal;
@@ -27,87 +28,99 @@ public class MandelbrotMovie {
         return new FunctionInterpolator(start, end, x -> x * x * x * (x * (x * 6 - 15) + 10));
     };
 
-    public static void main(String[] args) {
-        PaletteFactory paletteFactory = new PaletteFactory();
 
+    private final static double EASE_POWER = 7; // must be odd number
+    private static InterpolatorFactory<BigDecimal> EASE_IN = (BigDecimal start, BigDecimal end) -> {
+        return new FunctionInterpolator(start, end, x -> Math.pow(x, EASE_POWER));
+    };
+
+    private static InterpolatorFactory<BigDecimal> EASE_OUT = (BigDecimal start, BigDecimal end) -> {
+        return new FunctionInterpolator(start, end, x -> Math.pow(x - 1, EASE_POWER) + 1);
+    };
+
+
+    public static void main(String[] args) {
         List<MovieStep> movieSteps = new ArrayList<>();
-        movieSteps.add(new MovieStep(
-                new BigDecimal("0"),
-                new BigDecimal("0"),
-                new BigDecimal("0"),
-                paletteFactory.createPalette(PaletteType.RandomColor, 14, 20)));
-        // thorns
-        movieSteps.add(new MovieStep(
-                new BigDecimal("0.017919288259557892593847458731170858208746794667084869140010189701941671977885175491969822815680324832650288118287542225754354320968861805008056865043840568408674639001904242945377670838006845358428388295630356802224461622401647761424530072"),
-                new BigDecimal("1.01176097531987061853463090956462940839175503684365364035585094020187264122252703179366559338808085425394834006826628793779676461877510495451535804537480898432580908583825399893338707858055917113599638919828012253992937320289924731178786342"),
-                new BigDecimal("1"),
-                paletteFactory.createPalette(PaletteType.RandomColor, 14, 20)));
-        movieSteps.add(new MovieStep(
-                new BigDecimal("0.017919288259557892593847458731170858208746794667084869140010189701941671977885175491969822815680324832650288118287542225754354320968861805008056865043840568408674639001904242945377670838006845358428388295630356802224461622401647761424530072"),
-                new BigDecimal("1.01176097531987061853463090956462940839175503684365364035585094020187264122252703179366559338808085425394834006826628793779676461877510495451535804537480898432580908583825399893338707858055917113599638919828012253992937320289924731178786342"),
-                new BigDecimal("11"),
-                paletteFactory.createPalette(PaletteType.Fire, 1, 60)));
-        movieSteps.add(new MovieStep(
-                new BigDecimal("0.017919288259557892593847458731170858208746794667084869140010189701941671977885175491969822815680324832650288118287542225754354320968861805008056865043840568408674639001904242945377670838006845358428388295630356802224461622401647761424530072"),
-                new BigDecimal("1.01176097531987061853463090956462940839175503684365364035585094020187264122252703179366559338808085425394834006826628793779676461877510495451535804537480898432580908583825399893338707858055917113599638919828012253992937320289924731178786342"),
-                new BigDecimal("1"),
-                paletteFactory.createPalette(PaletteType.Fire, 1, 60)));
-        // snail shell
-        movieSteps.add(new MovieStep(
-                new BigDecimal("1.749721929742338571328512183204793465117897644259904770681747353482121708665972660839800936317633296441980469665826685985285388"),
-                new BigDecimal("-0.000029016647753686084545360932647113026525960648184743451989371745927172996759411354785502498950354939680922076169129062986700"),
-                new BigDecimal("1"),
-                paletteFactory.createPalette(PaletteType.Water, 1, 20)));
-        movieSteps.add(new MovieStep(
-                new BigDecimal("1.749721929742338571328512183204793465117897644259904770681747353482121708665972660839800936317633296441980469665826685985285388"),
-                new BigDecimal("-0.000029016647753686084545360932647113026525960648184743451989371745927172996759411354785502498950354939680922076169129062986700"),
-                new BigDecimal("11.0"),
-                paletteFactory.createPalette(PaletteType.Water, 1, 150)));
-        movieSteps.add(new MovieStep(
-                new BigDecimal("1.749721929742338571328512183204793465117897644259904770681747353482121708665972660839800936317633296441980469665826685985285388"),
-                new BigDecimal("-0.000029016647753686084545360932647113026525960648184743451989371745927172996759411354785502498950354939680922076169129062986700"),
-                new BigDecimal("11.3"),
-                paletteFactory.createPalette(PaletteType.RandomColor, 1, 5)));
-        movieSteps.add(new MovieStep(
-                new BigDecimal("1.749721929742338571328512183204793465117897644259904770681747353482121708665972660839800936317633296441980469665826685985285388"),
-                new BigDecimal("-0.000029016647753686084545360932647113026525960648184743451989371745927172996759411354785502498950354939680922076169129062986700"),
-                new BigDecimal("1.0"),
-                paletteFactory.createPalette(PaletteType.RandomColor, 1, 5)));
-        movieSteps.add(new MovieStep(
-                new BigDecimal("0"),
-                new BigDecimal("0"),
-                new BigDecimal("0"),
-                paletteFactory.createPalette(PaletteType.RandomColor, 14, 20)));
+        List<PointOfInterest> pointsOfInterest = travellingSalesman(StandardPointsOfInterest.POINTS_OF_INTEREST);
+        for (PointOfInterest poi : pointsOfInterest) {
+            movieSteps.add(toMovieStep(poi));
+        }
+        movieSteps.add(toMovieStep(pointsOfInterest.get(0)));
 
         createMovie(movieSteps, 100);
     }
 
+    private static List<PointOfInterest> travellingSalesman(PointOfInterest[] pointsOfInterest) {
+        return travellingSalesman(pointsOfInterest);
+    }
+
+    private static MovieStep toMovieStep(PointOfInterest poi) {
+        PaletteFactory paletteFactory = new PaletteFactory();
+        Palette palette = paletteFactory.createPalette(poi.paletteType, poi.paletteSeed, poi.paletteStep);
+        return new MovieStep(poi.x, poi.y, BigDecimal.valueOf(poi.zoom), palette);
+    }
+
     private static void createMovie(List<MovieStep> movieSteps, int frames) {
+        BigDecimal intermediateZoom = BigDecimal.valueOf(0.5);
+
         int imageIndex = 0;
         MovieStep lastStep = null;
         for (MovieStep currentStep: movieSteps) {
             if (lastStep != null) {
-                Interpolator<BigDecimal> xInterpolator = currentStep.positionInterpolatorFactory.create(lastStep.x, currentStep.x);
-                Interpolator<BigDecimal> yInterpolator = currentStep.positionInterpolatorFactory.create(lastStep.y, currentStep.y);
-                Interpolator<BigDecimal> zoomInterpolator = currentStep.zoomInterpolatorFactory.create(lastStep.zoom, currentStep.zoom);
+                if (needIntermediateStep(lastStep, currentStep)) {
+                    MovieStep half1Step = new MovieStep(lastStep.x, lastStep.y, intermediateZoom, lastStep.palette);
+                    imageIndex = interpolateBetweenMovieSteps(frames, imageIndex, lastStep, half1Step, SMOOTH, SMOOTH); // easeInOrOut(lastStep.zoom, halfStepZoom)
 
-                Path outDir = Paths.get("images", "zoom");
-                outDir.toFile().mkdirs();
+                    MovieStep half2Step = new MovieStep(currentStep.x, currentStep.y, intermediateZoom, lastStep.palette);
+                    imageIndex = interpolateBetweenMovieSteps(frames, imageIndex, half1Step, half2Step, SMOOTH, SMOOTH);
 
-                for (int frame = 0; frame < frames; frame++) {
-                    double value = ((double)frame) / frames;
-                    BigDecimal x = xInterpolator.interpolate(value);
-                    BigDecimal y = yInterpolator.interpolate(value);
-                    BigDecimal zoom = zoomInterpolator.interpolate(value);
-
-                    System.out.println("IMAGE " + imageIndex + " x=" + x + " y=" + y + " zoom=" + zoom);
-                    Palette palette = new MixPalette(lastStep.palette, currentStep.palette, value);
-                    renderImage(outDir, imageIndex, x, y, zoom, palette);
-                    imageIndex++;
+                    imageIndex = interpolateBetweenMovieSteps(frames, imageIndex, half2Step, currentStep, SMOOTH, SMOOTH);
+                } else {
+                    imageIndex = interpolateBetweenMovieSteps(frames, imageIndex, lastStep, currentStep, SMOOTH, SMOOTH);
                 }
             }
             lastStep = currentStep;
         }
+    }
+
+    private static boolean needIntermediateStep(MovieStep firstStep, MovieStep secondStep) {
+        return true;
+    }
+
+    private static InterpolatorFactory<BigDecimal> easeInOrOut(BigDecimal startZoom, BigDecimal endZoom) {
+        if (startZoom.compareTo(endZoom) <= 0) {
+            return EASE_OUT;
+        } else {
+            return EASE_IN;
+        }
+    }
+
+    private static int interpolateBetweenMovieSteps(
+            int frames,
+            int imageIndex,
+            MovieStep lastStep,
+            MovieStep currentStep,
+            InterpolatorFactory<BigDecimal> positionInterpolatorFactory,
+            InterpolatorFactory<BigDecimal> zoomInterpolatorFactory) {
+        Interpolator<BigDecimal> xInterpolator = positionInterpolatorFactory.create(lastStep.x, currentStep.x);
+        Interpolator<BigDecimal> yInterpolator = positionInterpolatorFactory.create(lastStep.y, currentStep.y);
+        Interpolator<BigDecimal> zoomInterpolator = zoomInterpolatorFactory.create(lastStep.zoom, currentStep.zoom);
+
+        Path outDir = Paths.get("images", "zoom");
+        outDir.toFile().mkdirs();
+
+        for (int frame = 0; frame < frames; frame++) {
+            double value = ((double)frame) / frames;
+            BigDecimal x = xInterpolator.interpolate(value);
+            BigDecimal y = yInterpolator.interpolate(value);
+            BigDecimal zoom = zoomInterpolator.interpolate(value);
+
+            System.out.println("IMAGE " + imageIndex + " x=" + x + " y=" + y + " zoom=" + zoom);
+            Palette palette = new MixPalette(lastStep.palette, currentStep.palette, value);
+            renderImage(outDir, imageIndex, x, y, zoom, palette);
+            imageIndex++;
+        }
+        return imageIndex;
     }
 
     private static void renderImage(Path outDir, int imageIndex, BigDecimal x, BigDecimal y, BigDecimal zoom, Palette palette) {
@@ -128,16 +141,24 @@ public class MandelbrotMovie {
 
     public static class FunctionInterpolator implements Interpolator<BigDecimal> {
         private final BigDecimal start;
+        private final BigDecimal end;
         private DoubleFunction<Double> function;
         private final BigDecimal range;
 
         public FunctionInterpolator(BigDecimal start, BigDecimal end, DoubleFunction<Double> function) {
             this.start = start;
+            this.end = end;
             this.range = end.subtract(start);
             this.function = function;
         }
         @Override
         public BigDecimal interpolate(double x) {
+            if (x <= 0.0) {
+                return start;
+            }
+            if (x >= 1.0) {
+                return end;
+            }
             return range.multiply(BigDecimal.valueOf(function.apply(x))).add(start);
         }
     }
@@ -150,23 +171,12 @@ public class MandelbrotMovie {
         public final BigDecimal x;
         public final BigDecimal y;
         public final BigDecimal zoom;
-        public final InterpolatorFactory<BigDecimal> positionInterpolatorFactory;
-        public final InterpolatorFactory<BigDecimal> zoomInterpolatorFactory;
-        private Palette palette;
+        public final Palette palette;
 
         public MovieStep(BigDecimal x, BigDecimal y, BigDecimal zoom, Palette palette) {
-            this(x, y, zoom, SMOOTHER, SMOOTHER, palette);
-        }
-
-        public MovieStep(BigDecimal x, BigDecimal y, BigDecimal zoom,
-                         InterpolatorFactory<BigDecimal> positionInterpolatorFactory,
-                         InterpolatorFactory<BigDecimal> zoomInterpolatorFactory,
-                         Palette palette) {
             this.x = x;
             this.y = y;
             this.zoom = zoom;
-            this.positionInterpolatorFactory = positionInterpolatorFactory;
-            this.zoomInterpolatorFactory = zoomInterpolatorFactory;
             this.palette = palette;
         }
     }
